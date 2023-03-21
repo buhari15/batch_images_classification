5396a72759db1ae09273cfcdc077cf094031124a
buhariabubakar <KAduna14@>
2023-03-21 16:28:27 +0100

    updated

--- prediction.py
+++ prediction.py
@@ -44,7 +44,7 @@ def classify():
         data = {'images_name': ix,
                 'label': class_name,
                 'probability': class_likelihood,
-                'date': now.strftime("%d ""%b" " %G" " %H:%M")
+                'date': now()
                 }
 
         with open("batch_result.csv", 'a') as f:
